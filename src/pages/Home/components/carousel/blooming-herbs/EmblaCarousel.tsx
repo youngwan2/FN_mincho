acda3e77b47978ad3858c8cc8bece2691ece1646
youngwan2 <qodna25@gmail.com>
2025-02-18 20:27:31 +0900

    feat: 홈 화면 이달의 개화약초 슬라이드 섹션 추가

---
+++ src/pages/Home/components/carousel/blooming-herbs/EmblaCarousel.tsx
@@ -0,0 +1,43 @@
+import React, { useCallback } from 'react'
+import { EmblaOptionsType, EmblaCarouselType } from 'embla-carousel'
+import Autoplay from 'embla-carousel-autoplay'
+import useEmblaCarousel from 'embla-carousel-react'
+
+type PropType = {
+  slides: number[]
+  options?: EmblaOptionsType
+}
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay()])
+
+  const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
+    const autoplay = emblaApi?.plugins()?.autoplay
+    if (!autoplay) return
+
+    const resetOrStop =
+      autoplay.options.stopOnInteraction === false
+        ? autoplay.reset
+        : autoplay.stop
+
+    resetOrStop()
+  }, [])
+
+ 
+  return (
+    <section className="w-full h-[550px] relative">
+      <div className="overflow-hidden" ref={emblaRef}>
+        <div className="flex">
+          {slides.map((index) => (
+            <div className={`${index%2==0? 'mt-30':''}  md:w-[300px] w-xs ml-10 bg-amber-50 shrink-0 rounded-xl h-[300px]`} key={index}>
+              <div className="embla__slide__number">{index + 1}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default EmblaCarousel
