f84938dfdcdd47f4789be9c64f8f3b301a7a5fdf
youngwan2 <qodna25@gmail.com>
2025-02-18 13:02:31 +0900

    feat: 홈 화면 배너 및 슬라이드 추가

---
+++ src/components/carousel/EmblaCarousel.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { EmblaOptionsType } from 'embla-carousel'
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons
+} from './EmblaCarouselArrowButtons'
+import useEmblaCarousel from 'embla-carousel-react'
+import './base.css'
+import './embla.css'
+
+type PropType = {
+  slides: number[]
+  options?: EmblaOptionsType
+}
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props
+  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi)
+
+  return (
+    <section className="embla bg-amber-100">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide" key={index}>
+              <div className="embla__slide__number">{index + 1}</div>
+            </div>
+          ))}
+        </div>
+
+      </div>
+
+      <div className="embla__buttons">
+        <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+        <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+      </div>
+    </section>
+  )
+}
+
+export default EmblaCarousel
