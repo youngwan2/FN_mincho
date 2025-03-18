807dd7ddc6c2bb0a6b84a7665833753153da974d
youngwan2 <qodna25@gmail.com>
2025-03-18 13:02:46 +0900

    feat: 페이지네이션 기능 추가

--- src/pages/Community/page.tsx
+++ src/pages/Community/page.tsx
@@ -3,33 +3,48 @@ import CommunityHeader from "./components/CommunityHeader";
 import CommunitySidebar from "./components/CommunitySidebar";
 import CommunityPost from "./components/CommunityPost";
 import CommunityBody from "./components/CommunityBody";
+import Pagination from "../../components/pagination/Pagination";
 import { usePostsGetQuery, usePostStatisticsGetQuery } from "../../hooks/queries/useQueryPosts";
-import { PostFetchState } from "../../types/post.types";
+import { PostFetchState, PostStatistics } from "../../types/post.types";
+
 
 const pageSize = 10
 export default function CommunityPage() {
 
-    const [page, setPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
     // 현재 선택된 카테고리 상태
     const [activeCategory, setActiveCategory] = useState<string>('free');
 
 
-    const { categoryInfos } = usePostStatisticsGetQuery(page, pageSize)
+    const { categoryInfos } = usePostStatisticsGetQuery(currentPage, pageSize)
     const conditions = {
         orderBy: 'asc',
         category: activeCategory
 
     }
-    const { posts, isError, isLoading, status } = usePostsGetQuery(page, pageSize, conditions)
+    const { posts, isError, isLoading, status } = usePostsGetQuery(currentPage, pageSize, conditions)
 
-    const postFetchState:PostFetchState = {
+    const postFetchState: PostFetchState = {
         isError,
         isLoading,
         status
     }
 
+    // 페이지네이션 메타데이터
+    const totalItems = calculateTotalItems(categoryInfos)
+    const perPage = pageSize;
+    const totalPage = Math.ceil(totalItems / perPage)
+
+    // 카테고리 선택 함수 
     const onCategoryHandler = (categoryId: string) => {
         setActiveCategory(categoryId)
+
+    }
+
+    // 페이지 변경 함수
+    const onPageChange = ({selected}:{selected:number}) => {
+        console.log("이동 페이지:", selected)
+        setCurrentPage(selected)
 
     }
     return (
@@ -43,10 +58,26 @@ export default function CommunityPage() {
                     {/* 카테고리 사이드바 */}
                     <CommunitySidebar activeCategory={activeCategory} categoryInfos={categoryInfos} onClick={onCategoryHandler} />
 
-                    {/* 게시판 내용 */}
-                    <CommunityPost postFetchState={postFetchState} activeCategory={activeCategory} categoryInfos={categoryInfos} posts={posts} />
+                    <div className="flex flex-col w-full bg-white rounded-lg shadow-sm overflow-hidden py-3 ">
+                        {/* 게시판 내용 */}
+                        <CommunityPost postFetchState={postFetchState} activeCategory={activeCategory} categoryInfos={categoryInfos} posts={posts} />
+                        {/* 페이지네이션 */}
+                        <Pagination perPage={perPage} onPageChange={onPageChange} totalPage={totalPage} />
+                    </div>
                 </CommunityBody>
             </div>
         </div>
     )
+}
+
+
+function calculateTotalItems(categoryInfos: PostStatistics[]) {
+
+    let totalItems = 0;
+    categoryInfos.forEach(categoryInfo => {
+        totalItems += categoryInfo.count
+    })
+
+    return totalItems
+
 }
