17c3c20e24d4753b338c11d8dba79c6d8740961a
Kim Young Wan <qodna25@gmail.com>
2025-05-12 16:29:58 +0900

    deploy: frontend 배포

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 import { HerbSearchCondition } from "../types/herb.types"
 import { PostSearchCondition } from "../types/post.types"
 
-export const baseUrl = import.meta.env.MODE === 'development' ? 'http://localhost:8080/api/v1' : 'https://www.minchoherb.com'
+export const baseUrl = import.meta.env.MODE === 'development' ? 'http://localhost:8080/api/v1' : 'https://www.minchoherb.com/api/v1'
 
 
 export const apiRoutes = {
